d088f1ae1b063c4348c2a17eb1eb265a6dc6e1da
Abhilash S <abhilash18082002@gmail.com>
2024-09-04 18:15:01 +0530

    responsiveness

--- src/app/components/ChatBot.jsx
+++ src/app/components/ChatBot.jsx
@@ -13,10 +13,10 @@ function ChatBot() {
     let i = 0;
     let j = 0;
     let isDeleting = false;
-    const typingSpeed = 100;
-    const deletingSpeed = 50;
-    const pauseDuration = 500;
-
+    const typingSpeed = 100; // Speed at which characters are typed
+    const deletingSpeed = 50; // Speed at which characters are deleted
+    const pauseDuration = 500; // Pause between typing and deleting a word
+  
     const type = () => {
       let currentWord = words[i];
       if (isDeleting) {
@@ -40,20 +40,47 @@ function ChatBot() {
       }
       setTimeout(type, isDeleting ? deletingSpeed : typingSpeed);
     };
-
+  
     type();
   }, []);
+  
+
+  const handlefetchCoinDetails = async (prompt) => {
+    setLoading(true);
+    const API_URL = "https://cryptochatbotserver-production.up.railway.app";
+    try {
+      const { data } = await axios.post(
+        `${API_URL}/ai/chat`,
+        { prompt },
+        {
+          withCredentials: true,
+        }
+      );
+      const response = { message: data.message, role: "model" };
+
+      setResponses((prev) => [...prev, response]);
+
+      console.log("success", data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+
+      const errorMessage = { message: "An error occurred. Please try again.", role: "model" };
+      setResponses((prev) => [...prev, errorMessage]);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
-    <div className="chatbox blur-background large-shadow shadow-2xl shadow-white z-50 bg-[#000518] bg-opacity-70 w-[90vw] md:w-[70vw] lg:w-[40vw] pd-6 h-[85vh] rounded-3xl">
+    <div className="chatbox blur-background large-shadow shadow-2xl shadow-white z-50 bg-[#000518] bg-opacity-70 w-[90vw] sm:w-[80vw] md:w-[70vw] lg:w-[40vw] h-[85vh] max-h-[90vh] rounded-3xl p-4">
       <div className="h-[13%] pl-3 border-gray-700 flex flex-col gap-x-4 items-center">
-        <div className="mt-4 mx-auto text-lg font-semibold items-center">
-          <p className="max-w-lg text-3xl font-semibold leading-normal text-gray-900 dark:text-white">
-            AI-Chat Bot
+        <div className="mt-4 mx-auto text-center text-lg font-semibold items-center">
+          <p className="max-w-lg text-2xl md:text-3xl font-semibold leading-normal text-gray-900 dark:text-white">
+            Al-Chat Bot
           </p>
         </div>
-        <div className="mx-auto text-lg font-semibold items-center">
-          <p className="text-sm text-gray-400">Real-time crypto market data</p>
+        <div className="mx-auto text-center text-lg font-semibold items-center">
+          <p className="text-xs md:text-sm text-gray-400">Real-time crypto market data</p>
         </div>
       </div>
 
@@ -62,11 +89,11 @@ function ChatBot() {
           <div className="flex flex-col py-5 px-5 overflow-y-auto h-full custom-scrollbar">
             {responses.map((item, index) =>
               item.role === "user" ? (
-                <div className="self-end" key={index}>
+                <div className="self-end mb-2 mt-2" key={index}>
                   <PromptMessage message={item.message} />
                 </div>
               ) : (
-                <div className="self-start" key={index}>
+                <div className="self-start mt-2 mb-2" key={index}>
                   <ResponseMessage message={item.message} />
                 </div>
               )
@@ -77,13 +104,13 @@ function ChatBot() {
           <div className="w-full h-full flex flex-col justify-center items-center">
             <div className="mt-4 w-full h-full flex flex-col justify-center items-center">
               <img
-                className="rounded-md w-[150px] h-[150px] mx-auto"
+                className="rounded-md w-[100px] h-[100px] sm:w-[120px] sm:h-[120px] md:w-[150px] md:h-[150px] mx-auto"
                 src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Robot.png"
                 alt="Robot"
                 width="25"
                 height="25"
               />
-              <h1
+             <h1
                 id="typewriter"
                 className="relative top-0 w-fit min-h-[100px] py-4 justify-center flex bg-gradient-to-r items-center from-blue-500 via-teal-500 to-pink-500 bg-clip-text text-6xl font-extrabold text-transparent text-center select-auto  hidden sm:block"
               >
@@ -92,7 +119,9 @@ function ChatBot() {
               <p className="text-2xl font-bold text-white sm:hidden">
                 Welcome to crypto chatbot
               </p>
-              <p className="text-gray-500">Inquire about the market data</p>
+              <p className="text-gray-500 text-sm md:text-base">
+                Inquire about the market data
+              </p>
             </div>
           </div>
         )}
